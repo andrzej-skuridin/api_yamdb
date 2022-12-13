6a4774dacfda2bb8b522b57038707395b87f8108
Andrey Skuridin <morhond@gmail.com>
2022-12-13 13:14:53 +0300

    models v1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,
+                            verbose_name='Категория',
+                            unique=True)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200,
+                            verbose_name='Жанр',
+                            unique=True)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200,
+                            verbose_name='Название',
+                            unique=True)
+    year = models.DateTimeField(blank=True,
+                                null=True)
+
+    # TBA by another contributor (field type: ForeignKey)
+    # ---------------------------------------------------
+    rating = models.FloatField(blank=True,
+                               null=True)
+    # ---------------------------------------------------
+
+    description = models.CharField(max_length=200,
+                                   verbose_name='Описание')
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True,
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True,
+        verbose_name='Категория'
+    )
