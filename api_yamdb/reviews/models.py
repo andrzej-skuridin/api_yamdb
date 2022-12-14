119007db4d3b256c807f07868da28861066becf9
Andrey Ponomarev <ponomarev.a.p@gmail.com>
2022-12-14 19:09:49 +0300

    Модель User переопределена, админка кастомизирована. Есть миграции.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+CHOICES = (
+    ('user', 'пользователь'),
+    ('moderator', 'модератор'),
+    ('admin', 'администратор'),
+)
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        'email',
+        unique=True,
+        max_length=254,
+    )
+
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+    )
+
+    role = models.CharField(
+        max_length=9,
+        choices=CHOICES,
+        default='user',
+    )
