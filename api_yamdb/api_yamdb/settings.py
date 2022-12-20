be238d3672a80de5d750ab98c10f5ff6776e9a22
Andrey Skuridin <morhond@gmail.com>
2022-12-20 22:56:14 +0300

    Revert "fixes v4"
    This reverts commit c92940e26084a7126001126b86e72daae582de62.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djoser',
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
